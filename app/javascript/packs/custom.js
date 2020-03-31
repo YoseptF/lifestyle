3c3308d0f50734e37ec75a2a951604802701e0bd
Joseph Flores <yosept.flores@gmail.com>
2020-03-31 14:25:46 -0600

    I added some quality of life improvements, and also media queries :D

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,9 +1,8 @@
 require('./bling.js')
 
-document.addEventListener("turbolinks:load", function() {
-  console.log('dogo');
+document.addEventListener('turbolinks:load', function () {
   let errors = $$('.field_with_errors');
-  
+
 
   if (errors.length != 0) {
     errors.forEach(error => {
@@ -13,11 +12,26 @@ document.addEventListener("turbolinks:load", function() {
 
   let mainImage = $('.article-show > img');
 
-  if (mainImage != null){
+  if (mainImage != null) {
     console.log('I entered');
-    
+
     new simpleParallax(mainImage);
-  }else{
+  } else {
     console.log('no image');
   }
+
+  let burguer = $('.burguer');
+
+  let menu = $('.access');
+  
+  console.log(burguer, menu)
+
+  burguer.on('click', event => {
+    if (menu.style.right != "0px") {
+      menu.style.right = "0px"
+    } else {
+      menu.style.right = "-200px"
+    }
+  });
+
 })
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,4 +64,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.hosts << "ae4991b3.ngrok.io"
 end
